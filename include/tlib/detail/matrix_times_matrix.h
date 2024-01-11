36d8a43c7957c60bc80e89c6a787ebae1e975939
Cem Bassoy <cem.bassoy@gmail.com>
2024-01-11 16:43:16 +0100

    core: make cblas transpose and format wrapper structs compilable for clang. adjust mtm and mtv wrapper function interfaces.

--- include/tlib/detail/matrix_times_matrix.h
+++ include/tlib/detail/matrix_times_matrix.h
@@ -9,7 +9,7 @@
 
 
 #include "tags.h"
-#include "cases.h"
+#include "cases_ttm.h"
 
 
 // <cblas.h>
@@ -36,36 +36,36 @@ namespace tlib::detail {
 //enum CBLAS_TRANSPOSE { CblasNoTrans=111, CblasTrans=112};
 
 
-struct cblas_layout { static CBLAS_LAYOUT value; };
+struct cblas_layout {};
 
-struct cblas_row : public cblas_layout { static inline constexpr auto cblas_layout::value = CblasRowMajor; };
-struct cblas_col : public cblas_layout { static inline constexpr auto cblas_layout::value = CblasColMajor; };
+struct cblas_row : cblas_layout { static inline constexpr CBLAS_LAYOUT value = CblasRowMajor; };
+struct cblas_col : cblas_layout { static inline constexpr CBLAS_LAYOUT value = CblasColMajor; };
 
 struct cblas_trans { static CBLAS_TRANSPOSE value; };
 
-struct cblas_tr   : public cblas_trans { static inline constexpr auto cblas_trans::value = CblasTrans; };
-struct cblas_notr : public cblas_trans { static inline constexpr auto cblas_trans::value = CblasNoTrans; };
+struct cblas_tr   : public cblas_trans { static inline constexpr CBLAS_TRANSPOSE value = CblasTrans; };
+struct cblas_notr : public cblas_trans { static inline constexpr CBLAS_TRANSPOSE value = CblasNoTrans; };
 
 
 template<class layout_t, class transA_t, class transB_t>
-class mtm
+class MatrixTimesMatrix
 {
 public:
-    template<class value_t>
-    static inline void run(value_t* A, value_t* B, value_t* C,
-                           int m,      int n,      int k, 
-                           int lda,    int ldb,    int ldc)
+    template<class value_t, class size_t>
+    static inline void run(value_t *const A, value_t *const B, value_t * C,
+                           size_t m,      size_t n,      size_t k, 
+                           size_t lda,    size_t ldb,    size_t ldc)
                            { impl(m,n,k, A,lda, B,ldb, C,ldc); }
 private:
     static constexpr inline auto layout = layout_t::value;
     static constexpr inline auto transA = transA_t::value;
     static constexpr inline auto transB = transB_t::value;
 
-    template<class value_t>
-    static inline void impl(int m, int n, int k, 
-                            value_t* A, int lda,
-                            value_t* B, int ldb,
-                            value_t* C, int ldc)
+    template<class value_t, class size_t>
+    static inline void impl(size_t m, size_t n, size_t k, 
+                            value_t *const A, size_t lda,
+                            value_t *const B, size_t ldb,
+                            value_t * C, size_t ldc)
     {
         auto alpha = value_t(1.0);
         auto beta  = value_t(0.0);
@@ -77,27 +77,27 @@ private:
 };
 
 
-using mtm_row_tr1 = mtm < cblas_row, cblas_tr,   cblas_notr >;
-using mtm_row_tr2 = mtm < cblas_row, cblas_notr, cblas_tr   >;
-using mtm_row     = mtm < cblas_row, cblas_notr, cblas_notr >;
+using mtm_row_tr1 = MatrixTimesMatrix < cblas_row, cblas_tr,   cblas_notr >;
+using mtm_row_tr2 = MatrixTimesMatrix < cblas_row, cblas_notr, cblas_tr   >;
+using mtm_row     = MatrixTimesMatrix < cblas_row, cblas_notr, cblas_notr >;
 
-using mtm_col_tr1 = mtm < cblas_col, cblas_tr  , cblas_notr >;
-using mtm_col_tr2 = mtm < cblas_col, cblas_notr, cblas_tr   >;
-using mtm_col     = mtm < cblas_col, cblas_notr, cblas_notr >;
+using mtm_col_tr1 = MatrixTimesMatrix < cblas_col, cblas_tr  , cblas_notr >;
+using mtm_col_tr2 = MatrixTimesMatrix < cblas_col, cblas_notr, cblas_tr   >;
+using mtm_col     = MatrixTimesMatrix < cblas_col, cblas_notr, cblas_notr >;
 
 
 
 template<class layout_t>
-class mtv
+class MatrixTimesVector
 {
 public:
-    template<class value_t>
-    static inline void run(value_t* A, value_t* x, value_t* y, int m, int n, int lda){ impl(m,n,k, A,lda, x, y); }
+    template<class value_t, class size_t >
+    static inline void run(value_t *const A, value_t *const x, value_t * y, size_t m, size_t n, size_t lda){ impl(m,n, A,lda, x, y); }
 private:
     static constexpr inline auto layout = layout_t::value;
 
-    template<class value_t>
-    static inline void impl(int m, int n, int k, value_t* A, int lda, value_t* x, value_t* y)
+    template<class value_t, class size_t>
+    static inline void impl(size_t m, size_t n, value_t *const A, size_t lda, value_t *const x, value_t * y)
     {
 				// CblasColMajor CblasNoTrans      m         n     alpha  a   lda   x  incx  beta  y   incy    
 				auto noTrA = cblas_notr::value;
@@ -112,8 +112,8 @@ private:
     }
 };
 
-using mtv_row = mtv<cblas_row>;
-using mtv_col = mtv<cblas_col>;
+using mtv_row = MatrixTimesVector <cblas_row>;
+using mtv_col = MatrixTimesVector <cblas_col>;
 
 
 template<class size_t>
@@ -134,12 +134,11 @@ inline void mtm(
 #endif 
 
 template<class value_t, class size_t>
-inline void mtm_row(
+inline void mtm_rm(
 			size_t const q, size_t const p,
-			value_t const*const a, size_t const*const na,     size_t const*const /*wa*/, size_t const*const pia,
-			value_t const*const b, size_t const*const nb, // is a row-major dense matrix
-			value_t      *const c, size_t const*const /*nc*/, size_t const*const /*wc*/, size_t const*const /*pic*/
-			)
+			value_t *const a, size_t const*const na, size_t const*const pia,
+			value_t *const b, size_t const*const nb, // is a row-major dense matrix
+			value_t * c )
 {
 
 
@@ -147,22 +146,22 @@ inline void mtm_row(
 	// B is always 
 	
 	auto n1 = na[0];
-	auto n2 = na[2];
+	auto n2 = na[1];
 	auto nq = na[q-1];
 	auto mq = nb[q-1];
   auto n  = num_elements(na,p) / nq;
 	
-	                                           // A,x,y, m,  n,  lda
-	     if(is_case_rm<1>(p,q,pia)) mtv_row     (b,a,c, mq, n1, n1  );
-                                             // a,b,c  m,  n,  k,  lda,ldb,ldc    	     
-	else if(is_case_rm<2>(p,q,pia)) mtm_row_tr1 (a,b,c, n2, mq, n1,  n1, mq, mq); // q=1 | A,C = CM | B = RM
-	else if(is_case_rm<3>(p,q,pia)) mtm_row     (b,a,c, mq, n1, n2,  n2, n1, n1); // q=2 | A,C = CM | B = RM
+	                                               // A,x,y, m,  n,  lda
+	     if(is_case_rm<1>(p,q,pia)) mtv_row::run     (b,a,c, mq, n1, n1  );
+                                                 // a,b,c  m,  n,  k,  lda,ldb,ldc    	     
+	else if(is_case_rm<2>(p,q,pia)) mtm_row_tr1::run (a,b,c, n2, mq, n1,  n1, mq, mq); // q=1 | A,C = CM | B = RM
+	else if(is_case_rm<3>(p,q,pia)) mtm_row::run     (b,a,c, mq, n1, n2,  n2, n1, n1); // q=2 | A,C = CM | B = RM
 	
-	else if(is_case_rm<4>(p,q,pia)) mtm_row     (b,a,c, mq, n2, n1,  n1, n2, n2); // q=1 | A,C = RM | B = RM
-	else if(is_case_rm<5>(p,q,pia)) mtm_row_tr2 (a,b,c, n1, mq, n2,  n2, mq, mq); // q=2 | A,C = RM | B = RM
+	else if(is_case_rm<4>(p,q,pia)) mtm_row::run     (b,a,c, mq, n2, n1,  n1, n2, n2); // q=1 | A,C = RM | B = RM
+	else if(is_case_rm<5>(p,q,pia)) mtm_row_tr2::run (a,b,c, n1, mq, n2,  n2, mq, mq); // q=2 | A,C = RM | B = RM
 	
-	else if(is_case_rm<6>(p,q,pia)) mtm_row_tr2 (a,b,c,  n, nq, nq,  nq, mq, mq); // q=pi(1) | A,C = RM | B = RM
-	else if(is_case_rm<7>(p,q,pia)) mtm_row     (b,a,c, mq,  n, nq,  nq,  n,  n); // q=pi(p) | A,C = RM | B = RM
+	else if(is_case_rm<6>(p,q,pia)) mtm_row_tr2::run (a,b,c,  n, nq, nq,  nq, mq, mq); // q=pi(1) | A,C = RM | B = RM
+	else if(is_case_rm<7>(p,q,pia)) mtm_row::run     (b,a,c, mq,  n, nq,  nq,  n,  n); // q=pi(p) | A,C = RM | B = RM
 	
 }
 
