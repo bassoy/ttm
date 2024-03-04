547843955bce91c6996878944d8fab0fa309a7d8
Cem Bassoy <cem.bassoy@gmail.com>
2024-03-04 15:27:52 +0100

    examples: update examples.

--- example/interface1.cpp
+++ example/interface1.cpp
@@ -1,9 +1,26 @@
 /*
-# include either -DUSE_OPENBLAS or -DUSE_INTELBLAS for fast execution
-g++ -I../include/ -std=c++17 -Ofast -fopenmp interface1.cpp -o interface1 && ./interface1
+# include MLK for fast execution
+
+MKL_ROOT_DIR="/opt/intel/oneapi"
+MKL_BLAS_DIR="${MKL_ROOT_DIR}/mkl/latest"
+MKL_COMP_DIR="${MKL_ROOT_DIR}/compiler/2023.2.0/linux/compiler"
+MKL_BLAS_INC="-I${MKL_BLAS_DIR}/include"
+MKL_BLAS_LIB="-Wl,--start-group ${MKL_BLAS_DIR}/lib/libmkl_intel_ilp64.a ${MKL_BLAS_DIR}/lib/libmkl_intel_thread.a ${MKL_BLAS_DIR}/lib/libmkl_core.a ${MKL_COMP_DIR}/lib/intel64_lin/libiomp5.a -Wl,--end-group -lpthread -lm -ldl -m64"
+MKL_BLAS_FLAGS="-DMKL_ILP64 -m64"
+
+
+TLIB_DIR=..
+TLIB_INC="-I${TLIB_DIR}/include"
+
+INCS="${TLIB_INC} ${MKL_BLAS_INC}"
+LIBS="${MKL_BLAS_LIB}"
+FLAGS="${MKL_BLAS_FLAGS} -DUSE_MKLBLAS"
+
+
+g++ ${INCS} -std=c++17 -Ofast -fopenmp interface1.cpp ${LIBS} ${FLAGS} -o interface1 && ./interface1
 */
 
-#include <tlib/ttv.h>
+#include <tlib/ttm.h>
 
 #include <vector>
 #include <numeric>
@@ -12,44 +29,63 @@ g++ -I../include/ -std=c++17 -Ofast -fopenmp interface1.cpp -o interface1 && ./i
 
 int main()
 {
-  using value_t    = float;
-  using tensor_t   = tlib::tensor<value_t>;     // or std::array<value_t,N>
-  using iterator_t = std::ostream_iterator<value_t>;
-  
-  auto mode = 2ul;
-  
-  auto A = tensor_t( {4,3,2}/*, {3,2,1}*/ ); 
-  auto B = tensor_t( {A.shape().at(mode-1),1} ); 
-  // strides are automatically computed. shape and layout verified.
-  
-  std::iota(A.begin(),A.end(),1);
-  std::fill(B.begin(),B.end(),1);
-  
-  std::cout << "A = [ "; std::copy(A.begin(), A.end(), iterator_t(std::cout, " ")); std::cout << " ];" << std::endl;
-  std::cout << "B = [ "; std::copy(B.begin(), B.end(), iterator_t(std::cout, " ")); std::cout << " ];" << std::endl;
+    using value_t    = float;
+    using tensor_t   = tlib::tensor<value_t>;     // or std::array<value_t,N>
+    using shape_t    = typename tensor_t::shape_t;
+
+    // shape tuple for A
+    auto na = shape_t{4,3,2};
+
+    // order of A and C
+    auto pa = na.size();
+
+    // contraction mode
+    auto q = 1ul;
+
+    // shape tuple for B
+    auto nb = shape_t{na[q-1]+1,na[q-1]};
+    auto pb = nb.size();
+
+    // layout tuple for A and C
+    auto pia = tlib::detail::generate_k_order_layout(pa,1ul);
+    auto pib = tlib::detail::generate_k_order_layout(pb,1ul);
+
+    auto A = tensor_t( na, pia );
+    auto B = tensor_t( nb, pib );
+
+    // strides are automatically computed. shape and layout verified.
+
+    std::iota(A.begin(),A.end(),1);
+    std::fill(B.begin(),B.end(),1);
+    
+    std::cout << "A = " << A << std::endl;
+    std::cout << "B = " << B << std::endl;
 
 /*
-  a = 
+  A =
   { 1  5  9  | 13 17 21
     2  6 10  | 14 18 22
     3  7 11  | 15 19 23
     4  8 12  | 16 20 24 };
 
-  b = { 1 1 1 } ;
+  B =
+  { 1  1  1  1  1
+    1  1  1  1  1
+    1  1  1  1  1
+    1  1  1  1  1};
 */
 
-  // correct shape, layout and strides of the output tensors C1,C2 are automatically computed and returned by the functions.  
-  // internally calls auto tlib::tensor_times_vector(mode, A,B, tlib::execution::blas, tlib::slicing::large, tlib::loop_fusion::all  );
-  auto C1 = A(mode) * B;
- 
-  std::cout << "C1 = [ "; std::copy(C1.begin(), C1.end(), iterator_t(std::cout, " ")); std::cout << " ];" << std::endl;
+    // correct shape, layout and strides of the output tensor C are automatically computed and returned by the functions.
+    // internally calls auto tlib::tensor_times_vector(mode, A,B, tlib::parallel_policy::omp_forloop_t{}, tlib::slicing_policy::slice_t{}, tlib::fusion_policy::all_t{}  );
+    auto C = A(q) * B;
 
-  
-/*
-  c = 
-  { 1+5+ 9 | 13+17+21
-    2+6+10 | 14+18+22
-    3+7+11 | 15+19+23
-    4+8+12 | 16+20+24 };
+    std::cout << "C = " << C << std::endl;
+
+
+/* for q=1
+  C =
+  { 1+..+4 5+..+8 9+..+12 | 13+..+16 17+..+20 21+..+24
+      ..     ..     ..    |    ..       ..       ..
+    1+..+4 5+..+8 9+..+12 | 13+..+16 17+..+20 21+..+24 };
 */
 }
--- example/interface2.cpp
+++ example/interface2.cpp
@@ -1,9 +1,16 @@
 /*
-# include either -DUSE_OPENBLAS or -DUSE_INTELBLAS for fast execution
-g++ -I../include/ -std=c++17 -Ofast -fopenmp interface2.cpp -o interface2 && ./interface2
+TLIB_DIR=..
+TLIB_INC="-I${TLIB_DIR}/include"
+
+INCS="${TLIB_INC} ${MKL_BLAS_INC}"
+LIBS="${MKL_BLAS_LIB}"
+FLAGS="${MKL_BLAS_FLAGS} -DUSE_MKLBLAS"
+
+
+g++ ${INCS} -std=c++17 -Ofast -fopenmp interface1.cpp ${LIBS} ${FLAGS} -o interface1 && ./interface1
 */
 
-#include <tlib/ttv.h>
+#include <tlib/ttm.h>
 
 #include <vector>
 #include <numeric>
@@ -12,45 +19,71 @@ g++ -I../include/ -std=c++17 -Ofast -fopenmp interface2.cpp -o interface2 && ./i
 
 int main()
 {
-  using value_t    = float;
-  using tensor_t   = tlib::tensor<value_t>;     // or std::array<value_t,N>
-  using iterator_t = std::ostream_iterator<value_t>;
-  
-  auto mode = 2ul;
-  
-  auto A = tensor_t( {4,3,2}/*, {3,2,1}*/ ); 
-  auto B = tensor_t( {A.shape().at(mode-1),1} ); 
-  // strides are automatically computed. shape and layout verified.
-  
-  std::iota(A.begin(),A.end(),1);
-  std::fill(B.begin(),B.end(),1);
-  
-  std::cout << "A = [ "; std::copy(A.begin(), A.end(), iterator_t(std::cout, " ")); std::cout << " ];" << std::endl;
-  std::cout << "B = [ "; std::copy(B.begin(), B.end(), iterator_t(std::cout, " ")); std::cout << " ];" << std::endl;
+    using value_t    = float;
+    using tensor_t   = tlib::tensor<value_t>;     // or std::array<value_t,N>
+    using shape_t    = typename tensor_t::shape_t;
 
-/*
-  a = 
-  { 1  5  9  | 13 17 21
-    2  6 10  | 14 18 22
-    3  7 11  | 15 19 23
-    4  8 12  | 16 20 24 };
+    // shape tuple for A
+    auto na = shape_t{4,3,2};
 
-  b = { 1 1 1 } ;
-*/
+    // order of A and C
+    auto pa = na.size();
+
+    // contraction mode
+    auto q = 1ul;
+
+    // shape tuple for B
+    auto nb = shape_t{na[q-1]+1,na[q-1]};
+    auto pb = nb.size();
+
+    // layout tuple for A and C
+    auto pia = tlib::detail::generate_k_order_layout(pa,1ul);
+    auto pib = tlib::detail::generate_k_order_layout(pb,1ul);
+
+    auto A = tensor_t( na, pia );
+    auto B = tensor_t( nb, pib );
+
+    // strides are automatically computed. shape and layout verified.
+
+    std::iota(A.begin(),A.end(),1);
+    std::fill(B.begin(),B.end(),1);
+
+    std::cout << "A = " << A << std::endl;
+    std::cout << "B = " << B << std::endl;
+
+    /*
+      A =
+      { 1  5  9  | 13 17 21
+        2  6 10  | 14 18 22
+        3  7 11  | 15 19 23
+        4  8 12  | 16 20 24 };
+
+      B =
+      { 1  1  1  1  1
+        1  1  1  1  1
+        1  1  1  1  1
+        1  1  1  1  1};
+    */
+
 
   // correct shape, layout and strides of the output tensors C1,C2 are automatically computed and returned by the functions.  
-  auto C1 = tlib::tensor_times_vector(mode, A,B, tlib::execution::seq , tlib::slicing::large, tlib::loop_fusion::none );  
-  auto C2 = tlib::tensor_times_vector(mode, A,B, tlib::execution::blas, tlib::slicing::small, tlib::loop_fusion::all  );
-  	
+    auto C1 = tlib::ttm(q, A,B, tlib::parallel_policy::threaded_gemm_t{} , tlib::slicing_policy::slice_t{},     tlib::fusion_policy::none_t{} );
+    auto C2 = tlib::ttm(q, A,B, tlib::parallel_policy::omp_forloop_t{}   , tlib::slicing_policy::slice_t{},     tlib::fusion_policy::all_t{} );
+    auto C3 = tlib::ttm(q, A,B, tlib::parallel_policy::omp_forloop_t{}   , tlib::slicing_policy::subtensor_t{}, tlib::fusion_policy::outer_t{} );
+    auto C4 = tlib::ttm(q, A,B, tlib::parallel_policy::batched_gemm_t{}  , tlib::slicing_policy::subtensor_t{}, tlib::fusion_policy::outer_t{} );
 
-  std::cout << "C1 = [ "; std::copy(C1.begin(), C1.end(), iterator_t(std::cout, " ")); std::cout << " ];" << std::endl;
-  std::cout << "C2 = [ "; std::copy(C2.begin(), C2.end(), iterator_t(std::cout, " ")); std::cout << " ];" << std::endl;
+
+    std::cout << "C1 = " << C1 << std::endl;
+    std::cout << "C2 = " << C2 << std::endl;
+    std::cout << "C3 = " << C3 << std::endl;
+    std::cout << "C4 = " << C4 << std::endl;
+
   
-/*
-  c = 
-  { 1+5+ 9 | 13+17+21
-    2+6+10 | 14+18+22
-    3+7+11 | 15+19+23
-    4+8+12 | 16+20+24 };
-*/
+
+    /* for q=1
+      C =
+      { 1+..+4 5+..+8 9+..+12 | 13+..+16 17+..+20 21+..+24
+          ..     ..     ..    |    ..       ..       ..
+        1+..+4 5+..+8 9+..+12 | 13+..+16 17+..+20 21+..+24 };
+    */
 }
