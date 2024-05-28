d817481c300a4cd6f9f1cbbb2072d73fe99d825d
Cem Bassoy <cem.bassoy@gmail.com>
2024-05-29 00:53:11 +0200

    add forward and backward ttms in ttmpy python wrapper.

--- ttmpy/src/wrapped_ttm.cpp
+++ ttmpy/src/wrapped_ttm.cpp
@@ -32,7 +32,7 @@ ttm(std::size_t const contraction_mode,
   auto const p = std::size_t(ainfo.ndim); //py::ssize_t  
 
 	if(p==0)        throw std::invalid_argument("Error calling ttmpy::ttm: first input should be a tensor with order greater than zero.");
-	if(q==0 || q>p) throw std::invalid_argument("Error calling ttmpy::ttm: contraction mode should be greater than zero or less than or equal to p.");  
+	if(q==0 || q>p) throw std::invalid_argument("Error calling ttmpy::ttm: contraction mode should be greater than zero and at most equal to p.");  
   
   auto const*const aptr = static_cast<T const*const>(ainfo.ptr);    // extract data an shape of input array  
   auto na = std::vector<std::size_t>(ainfo.shape  .begin(), ainfo.shape  .end());
@@ -72,15 +72,154 @@ ttm(std::size_t const contraction_mode,
                                bptr, nb.data(),            pib.data(), 
                                cptr, nc.data(), wc.data());
 #else 
-  assert(0);
+  static_assert(0,"Static assert in ttmpy/ttm: missing specification of define USE_OPENBLAS or USE_MKL");
 #endif
 
   return c;  
 }
 
 
+
+
+template<class T>
+py::array_t<T> 
+ttms(std::size_t const non_contraction_mode,
+     py::array_t<T> const& apy,
+     py::list const& bpy,
+     std::string morder
+    )
+{
+
+  if(morder!="optimal" && morder!="backward" && morder!="forward"){
+    throw std::invalid_argument("Error calling ttmpy::ttms: multiplication order should be either 'optimal', 'backward' or 'forward'.");
+  }
+
+  auto const q = non_contraction_mode;
+ 
+  auto const& ainfo = apy.request(); // request a buffer descriptor from Python of type py::buffer_info
+  auto const p = std::size_t(ainfo.ndim); //py::ssize_t
+
+	if(p==0)
+	  throw std::invalid_argument("Error calling ttmpy::ttms: input tensor order should be greater than zero.");
+	if(bpy.size() != p-1) 
+	  throw std::invalid_argument("Error calling ttmpy::ttms: number of input matrices is not equal to the tensor order - 1.");	
+	if(q==0 || q>p) 
+	  throw std::invalid_argument("Error calling ttmpy::ttms: non-contraction mode should be greater than zero and at most equal to p.");  
+
+  auto na = std::vector<std::size_t>(ainfo.shape.begin(), ainfo.shape.end());
+  
+  assert(na.size() == p);
+  
+  auto nb = std::vector<std::pair<std::size_t,std::size_t>>(p-1);
+  auto bs = std::vector<py::array_t<T>>(p-1);
+  // cast py::list of py::array_t to std::vector of py::array_t
+  std::transform(bpy.begin(), bpy.end(), bs.begin(), [](auto const& bj){ return py::cast<py::array_t<T>>(bj); }  );
+  
+  // check if all array orders are equal to 2 (need to be matrices)
+  auto all_dim_2 = std::all_of( bs.begin(), bs.end(), [](auto const& bj){ return bj.request().ndim == 2u; } ); 
+  if(!all_dim_2)
+    throw std::invalid_argument("Error calling ttmpy::ttms: some of the input matrices are not matrices.");  
+    
+  // copy vector dimensions to a separate container for convenience
+  std::transform( bs.begin(), bs.end(), nb.begin(), [](auto const& b){ return std::make_pair(b.request().shape[0],b.request().shape[1]); } );
+
+  // check if vector dimensions and corresponding tensor extents are the same
+  bool na_equal_nb_q_1 = q==1 || std::equal ( nb.begin()  , nb.begin()+(q-1), na.begin()    , [](auto const& l, auto const& r){ return l.second == r; } );
+  bool na_equal_nb_q_2 = q==p || std::equal ( nb.begin()+q, nb.end  ()      , na.begin()+q+1, [](auto const& l, auto const& r){ return l.second == r; } );  
+  if(!na_equal_nb_q_1 || !na_equal_nb_q_2) 
+    throw std::invalid_argument("Error calling ttmpy::ttms: vector dimension is not compatible with the dimension of a tensor mode.");  
+
+
+  // B[0]...B[p-2]
+  // r = 1,...,q-1,q+1,...,p <- contraction dimensions [1-based]
+  
+  
+  auto c = py::array_t<T>{};
+ 
+  // backward contractions  
+  if( morder == "backward" ){
+
+
+    auto r0 = q==p ? p-1 : p;
+    c  = ttm(r0, apy, bs[p-2]);
+    
+    if(q==p){
+      for(auto r = p-2; r > 0u ; --r)
+        c = ttm(r,c,bs[r-1]);
+    }
+    else{
+      for(auto r = p-1; r >  q; --r)
+        c = ttm(r,c,bs[r-2]);
+      for(auto r = q-1; r > 0; --r)
+        c = ttm(r,c,bs[r-1]);
+    }
+
+
+    
+  }
+  else if( morder == "forward" ){
+    auto r0 = q==1 ? 2u : 1u;
+    c  = ttm(r0, apy, bs[0]);
+    
+    if(q==1u){
+      for(auto r = 3u; r <= p; ++r)
+        c = ttm(r,c,bs[r-2]);
+    }
+    else{
+      for(auto r = 2u;  r <  q; ++r)
+        c = ttm(r,c,bs[r-1]);
+      for(auto r = q+1; r <= p; ++r)
+        c = ttm(r,c,bs[r-2]);
+    }
+  }
+  else /*if ( morder == "optimal" )*/ {  
+    
+    #if 0
+    
+    // copy references of all vectors and their contraction dimension.    
+    auto bpairs = std::vector<std::pair<py::array_t<T>*,unsigned>>(p-1);   
+    for(auto r = 1u; r < q; ++r) /* r = 1...q-1*/
+      bpairs.at(r-1) = std::make_pair(std::addressof(bs.at(r-1)),r);     
+    for(auto r = q+1; r <= p; ++r) /*r = q+1...p*/
+      bpairs.at(r-2) = std::make_pair(std::addressof(bs.at(r-2)),r);
+    
+    // sort (ascending)  all vector references according to their dimension
+    auto rhs_dim_is_larger = [](auto const& lhs, auto const& rhs){ return lhs.first->shape(0) < rhs.first->shape(0);};
+    std::sort(bpairs.begin(), bpairs.end(), rhs_dim_is_larger);
+    
+    // check if vectors are well sorted.
+    assert(std::is_sorted(bpairs.begin(), bpairs.end(), rhs_dim_is_larger));
+          
+    // update contraction modes for the remaining vectors after contraction
+    auto update_contraction_modes = [&bpairs](auto const& ib){
+      assert(ib != bpairs.rend());
+      auto const r = ib->second;
+      auto decrease_contraction = [r](auto &bpair){ if(bpair.second>r) --bpair.second; }; 
+      std::for_each(ib, bpairs.rend(), decrease_contraction);   
+    };
+    
+    auto ib = bpairs.rbegin();
+    c = ttm(ib->second, apy, *(ib->first));  
+    update_contraction_modes(ib);
+    
+    for(++ib; ib != bpairs.rend(); ++ib){
+      c = ttm(ib->second, c, *(ib->first));
+      update_contraction_modes(ib);
+    } 
+    
+    #endif
+
+  }
+
+  return c;
+  
+}
+
+
+
 PYBIND11_MODULE(ttmpy, m)
 {
   m.doc() = "python plugin ttmpy for fast execution of the tensor-times-matrix multiply";
-  m.def("ttm",  &ttm<double> , "computes the tensor-matrix product for the q-th mode", py::return_value_policy::move, py::arg("q"), py::arg("A"), py::arg("B"));  
+  m.def("ttm",  &ttm <double>, "computes the tensor-matrix product for the q-th mode",             py::return_value_policy::move, py::arg("q"), py::arg("A"), py::arg("B"));
+  m.def("ttms", &ttms<double>, "computes multiple tensor-matrix product except for the q-th mode", py::return_value_policy::move, py::arg("q"), py::arg("A"),py::arg("Bs"), py::arg("order")="forward");    
 }
--- ttmpy/tests/test.py
+++ ttmpy/tests/test.py
@@ -6,26 +6,87 @@ class TestTTM(unittest.TestCase):
   
   def test_ttm_mode1(self):
     A = np.arange(3*2*4, dtype=np.float64).reshape(3,2,4)
-    b = np.arange(5*3, dtype=np.float64).reshape(5,3)
-    D = np.einsum("ijk,xi->xjk", A, b)
-    C = tp.ttm(1, A, b)
+    B = np.arange(5*3, dtype=np.float64).reshape(5,3)
+    D = np.einsum("ijk,xi->xjk", A, B)
+    C = tp.ttm(1, A, B)
     self.assertTrue(np.all(C==D))
  
   def test_ttm_mode2(self):
     A = np.arange(3*2*4, dtype=np.float64).reshape(3,2,4)
-    b = np.arange(5*2, dtype=np.float64).reshape(5,2)
-    D = np.einsum("ijk,xj->ixk", A, b)
-    C = tp.ttm(2, A, b)
+    B = np.arange(5*2, dtype=np.float64).reshape(5,2)
+    D = np.einsum("ijk,xj->ixk", A, B)
+    C = tp.ttm(2, A, B)
     self.assertTrue(np.all(C==D))
 
 
   def test_ttm_mode3(self):
     A = np.arange(3*2*4, dtype=np.float64).reshape(3,2,4)
-    b = np.arange(5*4, dtype=np.float64).reshape(5,4)
-    D = np.einsum("ijk,xk->ijx", A, b)
-    C = tp.ttm(3, A, b)
+    B = np.arange(5*4, dtype=np.float64).reshape(5,4)
+    D = np.einsum("ijk,xk->ijx", A, B)
+    C = tp.ttm(3, A, B)
     self.assertTrue(np.all(C==D)) 
+
+
+
+class TestTTMs(unittest.TestCase): 
+  
+  def test_ttms_mode1(self):
+    A  = np.arange(3*2*4*5, dtype=np.float64).reshape(3, 2, 4, 5)
+    B = [np.arange(2*6, dtype=np.float64).reshape(6,2), np.arange(4*7, dtype=np.float64).reshape(7,4), np.arange(5*9, dtype=np.float64).reshape(9,5)]
     
+    #print(A.shape)
+    #print(B[0].shape)
+    #print(B[1].shape)
+    #print(B[2].shape)
+    
+    D  = np.einsum("ijkl,yj->iykl", A, B[0])
+    D  = np.einsum("ijkl,yk->ijyl", D, B[1])
+    D  = np.einsum("ijkl,yl->ijky", D, B[2])   
+    
+    for order in ["forward", "backward"] : #, "backward" , "optimal"
+      C = tp.ttms(1, A, B, order)
+      self.assertTrue(np.all(C==D))
+      
+  
+ 
+  def test_ttms_mode2(self):
+    A  = np.arange(3*2*4*5, dtype=np.float64).reshape(3, 2, 4, 5)
+    B = [np.arange(3*6, dtype=np.float64).reshape(6,3), np.arange(4*7, dtype=np.float64).reshape(7,4), np.arange(5*9, dtype=np.float64).reshape(9,5)]
+
+    D  = np.einsum("ijkl,yi->yjkl", A, B[0])
+    D  = np.einsum("ijkl,yk->ijyl", D, B[1])
+    D  = np.einsum("ijkl,yl->ijky", D, B[2])   
+
+    for order in ["forward", "backward"] : # , "backward", "optimal"
+      C = tp.ttms(2, A, B, order)
+      self.assertTrue(np.all(C==D))  
+
+
+  def test_ttms_mode3(self):
+    A  = np.arange(3*2*4*5, dtype=np.float64).reshape(3, 2, 4, 5)
+    B = [np.arange(3*6, dtype=np.float64).reshape(6,3), np.arange(2*7, dtype=np.float64).reshape(7,2), np.arange(5*9, dtype=np.float64).reshape(9,5)]
+    
+    D  = np.einsum("ijkl,yi->yjkl", A, B[0])
+    D  = np.einsum("ijkl,yj->iykl", D, B[1])
+    D  = np.einsum("ijkl,yl->ijky", D, B[2])
+
+    for order in ["forward", "backward"] : # , "backward", "optimal"
+      C = tp.ttms(3, A, B, order)
+      self.assertTrue(np.all(C==D))  
+
+
+  def test_ttms_mode4(self):
+    A  = np.arange(3*2*4*5, dtype=np.float64).reshape(3, 2, 4, 5)
+    B = [np.arange(3*6, dtype=np.float64).reshape(6,3), np.arange(2*7, dtype=np.float64).reshape(7,2), np.arange(4*9, dtype=np.float64).reshape(9,4)]
+    
+    D  = np.einsum("ijkl,yi->yjkl", A, B[0])
+    D  = np.einsum("ijkl,yj->iykl", D, B[1])
+    D  = np.einsum("ijkl,yk->ijyl", D, B[2])
+    
+    for order in ["forward", "backward"] : # , "backward", "optimal"
+      C = tp.ttms(4, A, B, order)
+      self.assertTrue(np.all(C==D)) 
+
     
 if __name__ == '__main__':
     unittest.main()
